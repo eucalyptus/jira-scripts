72551007ff08703d66a52673b13ab565df5a0f6b
Andy Grimm <agrimm@gmail.com>
2012-10-30 17:11:05 -0400

    Add customerreport script

---
+++ customerreport.py
@@ -0,0 +1,16 @@
+from jiraauth import jclient as jira
+
+'''
+Synopsis:  This script finds all issues with a "Customer:" snippet in a comment
+'''
+
+issues = jira.search_issues('''comment ~ "Customer"''')
+
+import re
+for x in issues:
+  comments = jira.comments(x.key)
+  for comment in comments:
+    for line in comment.body.split('\n'):
+      if re.match('.*Customer:.*', line):
+        print x.key + " : " + comment.author.displayName + " -- " + line
+        break
